414e80523e80980d6845bae522544023a37a4b1a
hodakoov <teeror93@gmail.com>
2023-05-20 13:53:39 +0400

    add form create user

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
 def signupuser(request):
-    return render(request, 'todo/signupuser.html')
+    return render(request, 'todo/signupuser.html', {'form': UserCreationForm})
