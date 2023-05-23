996f1432ead9fb793c807e11693ab32093095e49
hodakoov <teeror93@gmail.com>
2023-05-23 19:47:50 +0400

    Добавлен вход(loginuser) и проверка на наличие учетной записи.

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 
 def home(request):
@@ -27,6 +27,19 @@ def signupuser(request):
             return render(request, 'todo/signupuser.html',
                           {'form': UserCreationForm,
                            'error': 'Пароли не совпадают'})
+
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html',
+                          {'form': AuthenticationForm, 'error': 'Неправильный логин или пароль'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
 
 
 def logoutuser(request):
--- todo_project/urls.py
+++ todo_project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
 
     # Auth
     path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
 
     # Todo
